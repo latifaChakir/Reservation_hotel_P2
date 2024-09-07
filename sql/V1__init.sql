c178e3cf0072dc42b8210d0abdc0dab5c2d7c82d
latifaChakir <chakirlatifa2001@gmail.com>
2024-09-07 20:45:17 +0100

    added tables

--- sql/V1__init.sql
+++ sql/V1__init.sql
@@ -1,33 +1,31 @@
 CREATE TABLE clients (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    age INT ,
-    address VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(50) NOT NULL,
+                         age INT,
+                         address VARCHAR(100) NOT NULL,
+                         phone VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE hotel (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    phone VARCHAR(20) NOT NULL
+                       id SERIAL PRIMARY KEY,
+                       name VARCHAR(50) NOT NULL,
+                       address VARCHAR(100) NOT NULL,
+                       phone VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE chambre (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    numero INT NOT NULL UNIQUE,
-    isDisponible BIT DEFAULT 0
+                         id SERIAL PRIMARY KEY,
+                         numero INT NOT NULL UNIQUE,
+                         isDisponible BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE reservations (
-     id INT PRIMARY KEY AUTO_INCREMENT,
-     id_client INT,
-     id_chambre INT,
-     date_debut DATE NOT NULL,
-     date_fin DATE NOT NULL,
-     FOREIGN KEY (id_client) REFERENCES clients(id_client),
-     FOREIGN KEY (id_chambre) REFERENCES chambre(id),
-     CHECK (date_debut < date_fin)
+                              id SERIAL PRIMARY KEY,
+                              id_client INT,
+                              id_chambre INT,
+                              date_debut DATE NOT NULL,
+                              date_fin DATE NOT NULL,
+                              FOREIGN KEY (id_client) REFERENCES clients(id),
+                              FOREIGN KEY (id_chambre) REFERENCES chambre(id),
+                              CHECK (date_debut < date_fin)
 );
-
-
